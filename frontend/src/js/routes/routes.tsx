c7ab3015e7e75c89bc59990525b9754f55abfecc
TsuyoshiKaido <tsuyoshi.kaido@wealsoft.co.jp>
2023-08-09 16:24:50 +0900

    feature/FW-21 仮パスワードメール送信画面、パスワードリセット画面

--- frontend/src/js/routes/routes.tsx
+++ frontend/src/js/routes/routes.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import MainContainer from '../containers/container'
 import AuthentificationContainer from '../containers/authentification-container'
+import TemporaryPasswordContainer from '../containers/temporarypassword-container'
+import UpdatePasswordComponent from '../components/updatepassword-component'
+import updatepasswordContainer from '../containers/updatepassword-container'
 
 /**
  * Routing
@@ -14,6 +17,8 @@ export default () => {
       <Switch>
         <Route exact path='/fire-wall' component={MainContainer} />
         <Route exact path='/fire-wall/authentification' component={AuthentificationContainer} />
+        <Route exact path='/fire-wall/temporarypassword' component={TemporaryPasswordContainer} />
+        <Route exact path='/fire-wall/updatepassword' component={updatepasswordContainer} />
       </Switch>
     </BrowserRouter>
   )
